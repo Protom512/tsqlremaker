ffacb1b3c94848cafe8afc9d2daafc2653c59536
protom512 <protom512@noreply.github.com>
2024-06-14 11:15:43 +0000

    vscodeでの開発環境を一部作成

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 use nom::branch::alt;
 use nom::bytes::complete::{tag_no_case, take_while1};
 use nom::character::complete::{multispace0, multispace1};
+use nom::bytes::complete::{tag_no_case, take_while1};
+use nom::character::complete::{multispace0, multispace1};
 use nom::character::{is_alphanumeric, is_digit};
 use nom::combinator::{map, opt};
-use nom::{sequence::tuple, IResult};
+use nom::{
+    sequence::tuple,
+    IResult,
+};
 use regex::Regex;
 
 pub enum Operator {
@@ -19,25 +24,21 @@ pub enum Operator {
 /// expr ::= term   | term   ('+'|'-') term
 /// ```
 ///
-#[derive(Debug, PartialEq)]
-pub struct Color {
-    pub red: u8,
-    pub green: u8,
-    pub blue: u8,
-}
+
+
 
 pub struct Procedure<'a> {
     /// プロシージャ名
     proc_name: &'a str,
-    SQL_statement: SQL,
+    sql_statement: SQL,
 }
 #[derive(Debug, PartialEq)]
 pub struct SQL {
-    queries: Vec<query>,
+    queries: Vec<Query>,
 }
 #[derive(Debug, PartialEq)]
-pub struct query {
-    queries: Vec<query>,
+pub struct Query {
+    queries: Vec<Query>,
 }
 
 /// ```bnf
@@ -84,7 +85,7 @@ pub struct SP {}
 ///
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
-pub enum select_all_distict {
+pub enum SelectAllDistict {
     All,
     Distinct,
     None,
@@ -102,7 +103,30 @@ pub struct Select<'a> {
     order_by_clause: Option<&'a str>,
     compute_clause: Option<&'a str>,
 }
+// fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
+//     u8::from_str_radix(input, 16)
+// }
+//
+// fn is_hex_digit(c: char) -> bool {
+//     c.is_digit(16)
+// }
+//
+// fn hex_primary(input: &str) -> IResult<&str, u8> {
+//     map_res(take_while_m_n(2, 2, is_hex_digit), from_hex)(input)
+// }
+//
+// fn hex_color(input: &str) -> IResult<&str, Color> {
+//     let (input, _) = tag("#")(input)?;
+//     let (input, (red, green, blue)) = tuple((hex_primary, hex_primary, hex_primary))(input)?;
+//
+//     Ok((input, Color { red, green, blue }))
+// }
 
+/// .
+///
+/// # Errors
+///
+/// This function will return an error if .
 fn parse_select_into(input: &str) -> IResult<&str, &str> {
     let (input, opt_into) = opt(tuple((multispace0, tag_no_case("into"), multispace1)))(input)?;
     let into = match opt_into {
@@ -125,6 +149,10 @@ fn parse_select_top(input: &str) -> IResult<&str, Option<u8>> {
     let num: Option<u8> = opt_top.map(|(m, _, num, _)| num.parse().expect("failed to convert"));
     Ok((input, num))
 }
+
+
+
+
 
 fn parse_select_ditinct(input: &str) -> IResult<&str, &str> {
     let (input, opt_top) = opt(tuple((
@@ -155,15 +183,15 @@ fn parse_select(input: &str) -> IResult<&str, &str> {
     )(input)
 }
 fn remove_comments(input: &str) -> String {
-    let re = Regex::new(r"--.*\n?").unwrap();
+    let re = Regex::new(r"(?: (?:'[^']*?') | (?<singleline>--[^\n]*) | (?<multiline>(?:\/\*)+?[\w\W]+?(?:\*\/)+) )").unwrap();
     re.replace_all(input, "\n").into_owned()
 }
 
 fn sp(input: &str) -> IResult<&str, &str> {
     // let input_Str = remove_comments(input);
-    let (input, OB) = parse_select(input)?;
+    let (input, ob) = parse_select(input)?;
 
-    Ok((input, OB))
+    Ok((input, ob))
 }
 
 #[cfg(test)]
@@ -209,4 +237,6 @@ mod tests {
     fn test_rstest_parse_select_into(#[case] sql: &str, #[case] result: IResult<&str, &str>) {
         assert_eq!(parse_select_into(sql), result);
     }
+
+    // fn test_rstest_comments_()
 }
